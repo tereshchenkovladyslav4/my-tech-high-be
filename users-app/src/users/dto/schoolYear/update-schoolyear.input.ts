8d53064136dd88b5ca6e21159361a6a890d86576
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2022-04-18 09:52:14 +0800

    schoolYearEntity added grades, birth_date_ut, special_ed

--- users-app/src/users/dto/schoolYear/update-schoolyear.input.ts
+++ users-app/src/users/dto/schoolYear/update-schoolyear.input.ts
@@ -3,21 +3,37 @@ import { IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class UpdateSchoolYearInput {
+  @Field(() => Int)
+  @IsNotEmpty()
+  school_year_id?: number;
 
-    @Field(() => Int)
-    @IsNotEmpty()
-    school_year_id?: number;
-  
-    @Field(() => Date, { nullable: true})
-    date_begin?: string
+  @Field(() => Date, { nullable: true })
+  date_begin?: string;
 
-    @Field(() => Date, { nullable: true})
-    date_end?: string
+  @Field(() => Date, { nullable: true })
+  date_end?: string;
 
-    @Field(() => Date, { nullable: true})
-    date_reg_open?: string
+  @Field(() => Date, { nullable: true })
+  date_reg_open?: string;
 
-    @Field(() => Date, { nullable: true})
-    date_reg_close?: string
+  @Field(() => Date, { nullable: true })
+  date_reg_close?: string;
 
+  @Field(() => Number, { nullable: true })
+  midyear_application?: number;
+
+  @Field(() => Date, { nullable: true })
+  midyear_application_open?: string;
+
+  @Field(() => Date, { nullable: true })
+  midyear_application_close?: string;
+
+  @Field(() => String, { nullable: true })
+  birth_date_cut?: string;
+
+  @Field(() => Boolean, { nullable: true })
+  special_ed?: boolean;
+
+  @Field(() => String, { nullable: true })
+  grades?: string;
 }
